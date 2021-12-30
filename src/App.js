6efaf8c8c4d743bc36c5f8ae25e9231c327cbf32
mondriy <58882902+mondriy@users.noreply.github.com>
2021-12-30 20:28:53 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Snowfall from 'react-snowfall'
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [audio, setAudio] = useState("");
+  const [isPlay, setPlay] = useState(false);
+  const [className, setClassName] = useState('disk paused');
+
+  const Play = () => {
+    setClassName('disk');
+    setPlay(true);
+    audio.play();
+  };
+
+  const Stop = () => {
+    setClassName('disk paused');
+    setPlay(false);
+    audio.pause();
+  };
+
+  useEffect(() => {
+    setAudio(new Audio("Home Alone - Somewhere In My Memory.mp3"));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isPlay && <Snowfall />}
+      <div className={className} onClick={isPlay ? Stop : Play}></div>
+      <div className="name-container">
+        <div className="name">Один дома</div>
+      </div>
     </div>
   );
 }
